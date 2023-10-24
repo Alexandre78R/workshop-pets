9e2c8ccc8435891f5f9f4a7efa2f36b6a0d87336
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-10-24 16:20:28 +0200

    premier commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [first, setFirst] = useState(second)
+  useEffect(() => {
+  
+  }, [])
+  
+  return (
+    <>
+      <h1></h1>
+    </>
+  );
+}
+
+export default App;
