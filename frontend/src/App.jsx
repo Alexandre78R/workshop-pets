6b8e417532d401dbd2c7b827fdc8799fd310badf
Alexandre78R <alexandre.renard98@gmail.com>
2023-10-25 14:07:12 +0200

    correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,47 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+// import pets from "./data/pets.json";
+import PetCard from "./components/PetCard";
+// https://wild.creativebrain.fr/pets
 
 function App() {
-  const [pets, setPets] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3001/pets")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setPets(data);
-      });
-  }, []);
 
+  const [pets, setPets] = useState(null);
+
+  useEffect(() => {
+    fetch("https://wild.creativebrain.fr/pets")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setPets(data);
+    });
+  }, [])
+  
   return (
     <>
-      {/* <h1>{pets.length != 0 ? `${pets[2].name}` : "Chargement..."}</h1> */}
-      <h1>Liste : </h1>
+    <h1>Coucou</h1>
+    {
+      pets ? 
+      pets.map((pet, index) => {
+        return (
+          <div key={index}>
+            <PetCard
+              name={pet.name}
+              weight={pet.weight} 
+              photo={pet.photo} 
+              sleepAmount={pet.sleepAmount}
+              curious={pet.curious}
+            />
+          </div>
+        )
+      })
+      :
+      "Chargement ..."
+    }
 
-
-      {pets.map((pet) => {
-        return <p>{pet.name}</p>;
-      })}
+    
     </>
   );
-
-  // if (pets.length != 0) {
-  //   return (
-  //     <>
-  //       <h1>{pets[2].name}</h1>
-  //     </>
-  //   );
-  // } else {
-  //   return (
-  //     <>
-  //       <h1>Chargement...</h1>
-  //     </>
-  //   );
-  // }
 }
 
 export default App;
---
+++ frontend/src/components/PetCard.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+function PetCard (props) {
+    const [display, setDisplay] = useState(false);
+    const { name, weight, photo, sleepAmount, curious} = props;
+
+    function togleDisplay () {
+        setDisplay(!display)
+    }
+
+    return (
+        <>
+            <p>{name}</p>
+            {
+                display ?
+                <>
+                    <img src={photo.thumb} alt={name} />
+                    <p>{sleepAmount}</p>
+                    <p>{weight}</p>
+                </>
+                :
+                // ""
+                    null
+            }
+            {/* {
+                !display ?
+                null
+                :
+                <>
+                <img src={photo.thumb} alt={name} />
+                <p>{sleepAmount}</p>
+                <p>{weight}</p>
+                </>
+            } */}
+            <button onClick={togleDisplay}>{display ? "-" : "+"}</button>
+        </>
+    )
+}
+
+export default PetCard;
